035b2fbc0abf4487a8ea9d4e068b93d3294e2cc4
Error <null>
2025-05-06 01:30:22 +0300

    this is a tough one

--- src/app/(main)/addProduct/page.jsx
+++ src/app/(main)/addProduct/page.jsx
@@ -1,119 +1,159 @@
-"use client"
+"use client";
 
-import { useRef, useState, useEffect, useActionState } from "react"
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import { IoMdCloudUpload } from "react-icons/io"
-import { IoMdAdd } from "react-icons/io"
+import { useRef, useState, useEffect, useActionState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { IoMdCloudUpload } from "react-icons/io";
+import { IoMdAdd } from "react-icons/io";
 
-import PopUp from '@/components/pop_up'
-import { TbCongruentTo } from "react-icons/tb"
+import PopUp from "@/components/pop_up";
+import { TbCongruentTo } from "react-icons/tb";
 
 export default function () {
-  const [category, setCategory] = useState('')
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [time, setTime] = useState('')
-  const [price, setPrice] = useState('')
-  const [image, setImage] = useState(null)
-  const [error, setError] = useState({})
-  const [popError, setPopError] = useState(false)
+  const [category, setCategory] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [time, setTime] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState({});
+  const [popError, setPopError] = useState(false);
 
-  const fileInputRef = useRef(null)
-  const router = useRouter()
+  const fileInputRef = useRef(null);
+  const router = useRouter();
 
   const openFileDialogue = () => {
-    fileInputRef.current.click()
-  }
+    fileInputRef.current.click();
+  };
 
-  const [uploading, setUploading] = useState(false)
-  const [submitting, setSubmitting] = useState(false)
+  const [uploading, setUploading] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    setSubmitting(uploading)
-  }, [uploading])
+    setSubmitting(uploading);
+  }, [uploading]);
 
   const handleImageUpload = async (e) => {
-
-    const file = e.target.files[0]
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onload = async () => {
-      const base64Image = reader.result
-      setImage(base64Image)
-    }
-  }
+      const base64Image = reader.result;
+      setImage(base64Image);
+    };
+  };
 
   const handleSubmit = async () => {
-
-    let anyError = undefined
-    if (!image) { anyError = true; setError({ ...error, image: true }) }
-    if (!price) { anyError = true; setError({ ...error, price: true }) }
-    if (!time) { anyError = true; setError({ ...error, time: true }) }
-    if (!description) { anyError = true; setError({ ...error, description: true }) }
-    if (!title) { anyError = true; setError({ ...error, title: true }) }
-    if (!category) { anyError = true; setError({ ...error, category: true }) }
+    let anyError = undefined;
+    if (!image) {
+      anyError = true;
+      setError({ ...error, image: true });
+    }
+    if (!price) {
+      anyError = true;
+      setError({ ...error, price: true });
+    }
+    if (!time) {
+      anyError = true;
+      setError({ ...error, time: true });
+    }
+    if (!description) {
+      anyError = true;
+      setError({ ...error, description: true });
+    }
+    if (!title) {
+      anyError = true;
+      setError({ ...error, title: true });
+    }
+    if (!category) {
+      anyError = true;
+      setError({ ...error, category: true });
+    }
 
     if (anyError) {
-      setSubmitting(false)
-      setUploading(false)
-      return
+      setSubmitting(false);
+      setUploading(false);
+      return;
     }
 
-    setUploading(true)
+    setUploading(true);
 
     try {
-      const response = await fetch('/api/addProduct', {
-        method: 'PUT',
+      const response = await fetch("/api/addProduct", {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          category, title, description, time, price, image
-        })
-      })
+          category,
+          title,
+          description,
+          time,
+          price,
+          image,
+        }),
+      });
 
-      const { url } = await response.json()
-      router.push('/products')
+      const { url } = await response.json();
+      router.push("/products");
     } catch (error) {
-      setPopError(true)
-      setTimeout(() => setPopError(false), 8000)
+      setPopError(true);
+      setTimeout(() => setPopError(false), 8000);
     } finally {
-      setUploading(false)
+      setUploading(false);
     }
 
-    setUploading(false)
-  }
+    setUploading(false);
+  };
 
   return (
     <div className="p-4 text-white w-full">
-      {popError ? <div className="absolute bottom-0 -translate-y-16 w-full flex items-center justify-center">
-        <PopUp message={"Something went wrong. Could not add product"} />
-      </div> : null}
+      {popError ? (
+        <div className="absolute bottom-0 -translate-y-16 w-full flex items-center justify-center">
+          <PopUp message={"Something went wrong. Could not add product"} />
+        </div>
+      ) : null}
 
-      <form action={(e) => {
-        setSubmitting(true)
-        handleSubmit(e)
-      }} className="w-full flex flex-col gap-4 mb-20">
-        <div className="font-bold text-lg pl-3 translate-y-2">Select Category</div>
+      <form
+        action={(e) => {
+          setSubmitting(true);
+          handleSubmit(e);
+        }}
+        className="w-full flex flex-col gap-4 mb-20"
+      >
+        <div className="font-bold text-lg pl-3 translate-y-2">
+          Select Category
+        </div>
         <div className="">
           <select
-            name="category" id=""
+            name="category"
+            id=""
             className="bg-gray-800 rounded-xl px-3 py-2 border border-gray-600 outline-none"
             value={category}
             disabled={uploading}
-            onChange={e => {setCategory(e.target.value); setError({ ...error, category: undefined })}}
+            onChange={(e) => {
+              setCategory(e.target.value);
+              setError({ ...error, category: undefined });
+            }}
           >
-            <option disabled defaultChecked value="">Select Category</option>
+            <option disabled defaultChecked value="">
+              Select Category
+            </option>
             <option value="landscape">Landscape</option>
             <option value="portrait">Portrait</option>
             <option value="abstract">Abstract</option>
             <option value="modern">Modern</option>
           </select>
-          {error.category ? <div className="h-3 text-red-500 ml-2 mt-1">This field is required.</div> : null }
+          {error.category ? (
+            <div className="h-3 text-red-500 ml-2 mt-1">
+              This field is required.
+            </div>
+          ) : null}
         </div>
 
-        <div className="font-bold text-lg pl-3 translate-y-2">Title of your Product</div>
+        <div className="font-bold text-lg pl-3 translate-y-2">
+          Title of your Product
+        </div>
         <div className="">
           <input
             type="text"
@@ -122,9 +162,18 @@ export default function () {
             className="outline-none px-3 py-1  bg-gray-800 border border-gray-600 focus:outline focus:outline-gray-600 w-full rounded-xl"
             value={title}
             disabled={uploading}
-            onChange={e => {setTitle(e.target.value); setError({ ...error, title: undefined })}}
+            onChange={(e) => {
+              setTitle(e.target.value);
+              setError({ ...error, title: undefined });
+            }}
           />
-          {error.title ? <div className="text-red-500 ml-2 mt-1 h-3">This field is required.</div> : <div></div>}
+          {error.title ? (
+            <div className="text-red-500 ml-2 mt-1 h-3">
+              This field is required.
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
 
         <div className="font-bold text-lg pl-3 translate-y-2">Description</div>
@@ -136,12 +185,23 @@ export default function () {
             className="outline-none px-3 py-1 bg-gray-800 border border-gray-600 focus:outline focus:outline-gray-600 w-full rounded-xl"
             value={description}
             disabled={uploading}
-            onChange={e => {setDescription(e.target.value); setError({ ...error, description: undefined })}}
+            onChange={(e) => {
+              setDescription(e.target.value);
+              setError({ ...error, description: undefined });
+            }}
           />
-          {error.description ? <div className="h-3 text-red-500 ml-2 mt-1">This field is required.</div> : <div className=""></div>}
+          {error.description ? (
+            <div className="h-3 text-red-500 ml-2 mt-1">
+              This field is required.
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
         </div>
 
-        <div className="font-bold text-lg pl-3 translate-y-2">Delivery Time</div>
+        <div className="font-bold text-lg pl-3 translate-y-2">
+          Delivery Time
+        </div>
         <div>
           <input
             type="text"
@@ -150,9 +210,18 @@ export default function () {
             className="outline-none px-3 py-1 bg-gray-800 border border-gray-600 focus:outline focus:outline-gray-600 w-full rounded-xl"
             value={time}
             disabled={uploading}
-            onChange={e => {setTime(e.target.value); setError({ ...error, time: undefined })}}
+            onChange={(e) => {
+              setTime(e.target.value);
+              setError({ ...error, time: undefined });
+            }}
           />
-          {error.time ? <div className="h-3 text-red-500 ml-2 mt-1">This field is required.</div> : <div className=""></div>}
+          {error.time ? (
+            <div className="h-3 text-red-500 ml-2 mt-1">
+              This field is required.
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
         </div>
 
         <div className="font-bold text-lg pl-3 translate-y-2">Price</div>
@@ -164,15 +233,27 @@ export default function () {
             className="outline-none px-3 py-1 bg-gray-800 border border-gray-600 focus:outline focus:outline-gray-600 w-full rounded-xl"
             value={price}
             disabled={uploading}
-            onChange={e => {setPrice(e.target.value); setError({ ...error, price: undefined })}}
+            onChange={(e) => {
+              setPrice(e.target.value);
+              setError({ ...error, price: undefined });
+            }}
           />
-          {error.price ? <div className="h-3 text-red-500 ml-2 mt-1">This field is required.</div> : <div className=""></div>}
+          {error.price ? (
+            <div className="h-3 text-red-500 ml-2 mt-1">
+              This field is required.
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
         </div>
 
         <div className="font-bold text-lg pl-3 translate-y-2">Image</div>
-        <div onClick={openFileDialogue} className="h-52 overflow-hidden bg-gray-800 border border-gray-600 rounded-xl flex items-center justify-center">
-          {image
-            ? <Image
+        <div
+          onClick={openFileDialogue}
+          className="h-52 overflow-hidden bg-gray-800 border border-gray-600 rounded-xl flex items-center justify-center"
+        >
+          {image ? (
+            <Image
               src={image || null}
               alt=""
               width={300}
@@ -180,34 +261,46 @@ export default function () {
               className=""
               unoptimized
             />
-            : <div>
+          ) : (
+            <div>
               <IoMdCloudUpload size={30} color="gray" />
             </div>
-          }
-          <input type="file"
+          )}
+          <input
+            type="file"
             id="avatar-upload"
             className="hidden"
             accept="image/*"
             ref={fileInputRef}
-            onChange={(e) => {setError({ ...error, image: undefined }); handleImageUpload(e)}}
+            onChange={(e) => {
+              setError({ ...error, image: undefined });
+              handleImageUpload(e);
+            }}
             disabled={uploading}
           />
         </div>
-        {error.image ? <div className="text-red-500 ml-2 h-3 mt-1">This field is required.</div> : <div className=""></div>}
-        {submitting
-          ? <div className="self-end mr-2 p-1 rounded-full bg-purple-700">
+        {error.image ? (
+          <div className="text-red-500 ml-2 h-3 mt-1">
+            This field is required.
+          </div>
+        ) : (
+          <div className=""></div>
+        )}
+        {submitting ? (
+          <div className="self-end mr-2 p-1 rounded-full bg-purple-700">
             <div className="bg-gradient-to-tr from-gray-200 to-gray-800 p-1 rounded-full animate-spin ">
               <div className="bg-purple-700 rounded-full p-2"></div>
             </div>
           </div>
-          : <button type="submit"  className="p-1 self-end mr-2 bg-purple-700 rounded-full ">
+        ) : (
+          <button
+            type="submit"
+            className="p-1 self-end mr-2 bg-purple-700 rounded-full "
+          >
             <IoMdAdd size={26} color="white" />
           </button>
-        }
+        )}
       </form>
     </div>
-  )
+  );
 }
-
-
-
--- src/app/api/addProduct/route.js
+++ src/app/api/addProduct/route.js
@@ -1,27 +1,25 @@
-import { Product } from "@/db/db"
-import cloudinary from 'cloudinary'
+import { Product } from "@/db/db";
+import cloudinary from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export const PUT = async (req, res) => {
   try {
-    const body = await req.json()
-    console.log(body);
+    const body = await req.json();
+    console.log("contents of the body are", body);
     const result = await cloudinary.v2.uploader.upload(body.image, {
-      resource_type: 'auto',
+      resource_type: "auto",
     });
-    const url = result.secure_url
+    const url = result.secure_url;
 
-    await Product.insertOne({ ...body, image: url })
+    await Product.insertOne({ ...body, image: url });
 
-    return Response.json({ url })
+    return Response.json({ url });
   } catch (err) {
-    Response.json({ message: "Error: Couldn't upload image" })
+    return Response.json({ message: "Error: Couldn't upload image" });
   }
-}
-
+};
